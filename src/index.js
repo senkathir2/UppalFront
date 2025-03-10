688f2ca728e8a920a6544d1bf16ea421234a6a4b
senkathir2 <senkathir2@gmail.com>
2025-03-11 01:20:26 +0530

    keycloak-integ

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import Dashboard from "./Pages/Dashboard";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import UserService from "./Services/UserService";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+UserService.initKeycloak(root)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
